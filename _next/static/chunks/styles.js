b3f8d3a55acee2aa33365a1e5e557eaad8285ec1
Alihan Berk Bilgiç <alihanberkbilgic@Alihans-MacBook-Pro.local>
2019-02-18 12:08:16 +0300

    landing

--- _next/static/chunks/styles.js
+++ _next/static/chunks/styles.js
@@ -29,7 +29,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1550478297937");
+            injectCss(link, link.href.split("?")[0] + "?unix=1550480761579");
           });
       }
     }
--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0fa7de293f1eb1f64c64";
+/******/ 	var hotCurrentHash = "2f19c31e6f15c52023fb";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
